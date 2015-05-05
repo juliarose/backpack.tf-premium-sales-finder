f51dd301d32df5f39cda3b89791d543bd1c07ba4
Julia <juliarose@users.noreply.github.com>
2015-05-04 21:31:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# backpack.tf Premium Recent Sales Finder
+
+Used for quickly scanning items to find recent sales.
+
+## Requirements
+* [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (Chrome)
+* [Greasemonkey](https://addons.mozilla.org/en-us/firefox/addon/greasemonkey/) (Firefox)
+
+## Installation
+1. Ensure that you have either Tampermonkey or Greasemonkey installed.
+2. Download the script [`backpack.tf-historical-comparison.user.js`](backpack.tf-historical-comparison.user.js?raw=true).
+3. Confirm that you want to install the script.
+4. The script should now be installed and ready to use.
+
+## Updates
+* 1.0.0 - May 4th, 2015 - Initial Release
+
+## Overview
+
+![premium](/images/premium-showstopper-i-see-you.png?raw=true)
+
+Scanning for recent sales must be done manually. You can either click the "Check" label to check an item individually, or use the "Check sales for each item" shortcut in the upper right corner.
+
+Green indicates the sale occurred in the last 60 days (very recent). Yellow indiciates between 60 and 90 days (recent). Red indicates the last sale occurred more than 90 days ago or the item has never been exchanged.
+
+Occasionally an asterisk (*) will be aside the number of days. This means the item is duplicated and the number of days may be not accurate. Check with caution.
---
+++ backpacktf-premium-sales-finder.user.js
@@ -0,0 +1,172 @@
+// ==UserScript==
+// @name        backpack.tf Premium Recent Sales Finder
+// @namespace   http://steamcommunity.com/profiles/76561198080179568/
+// @author      Julia
+// @description Find recent sales for those hats :3
+// @include     /https?:\/\/backpack\.tf\/premium\/search.*/
+// @version     1.5a
+// @grant       none
+// ==/UserScript==
+
+var idcol = 0;
+
+_ready = function() {
+    var panel = $('#page-content .panel').last(),
+        checkSales = $('<a href="javascript:void(0)">Check sales for each item</a>');
+    
+    panel.find('table tr').each(function() {
+        if ($(this).find('td').length) {
+            var label = $('<a href="javascript:void(0)" class="check-sale"><span class="label label-info">Check</span></a>'),
+                td = $('<td class="sale" style="width:100px"/>').append(label);
+            
+            $(this).append(td);
+            
+            label.click(function() {
+                _labelClicked($(this));
+            });
+        } else if ($(this).find('th').length) {
+            $(this).find('th').each(function() {
+                if ($(this).text() == 'ItemID') {
+                    idcol = $(this).index();
+                }
+            });
+            
+            var th = $('<th>Last Sale</th>');
+            
+            $(this).append(th);
+        }
+    });
+    
+    panel.find('.panel-heading .pull-right a').replaceWith(checkSales);
+    
+    checkSales.click(function() {
+        $(this).remove();
+        
+        _checkSales();
+    });
+}
+
+_checkSales = function() {
+    var n = 0, gap = 300;
+    
+    $('.check-sale').each(function() {
+        var e = this;
+        
+        setTimeout(function() {
+            $(e).trigger('click');
+        }, n);
+        
+        n += gap;
+    });
+}
+
+_labelClicked = function(label) {
+    var id = label.closest('tr').find('td').eq(idcol).text();
+    
+    if (id) {
+        _ajax(id, label);
+    }
+}
+
+_ajax = function(id, label) {
+    $.ajax({
+        type: 'GET',
+        dataType: 'html',
+        url: _itemURL(id),
+        success: function (response) {
+            var doc = document.implementation.createHTMLDocument('item'); doc.documentElement.innerHTML = response;
+            var data = $(doc).find('#page-content');
+            
+            _checkHistory(data, label);
+        }, error: function (xhr, ajaxOptions, thrownError) {
+        }
+    });
+}
+
+_checkHistory = function(data, label) {
+    var itemidcol=0,lastseencol=0,ownercol=0,
+        previousOwner,previousDate,now=new Date(),
+        recentsale=false,hasSales=false,difference,days,al=$(data).find('.alert-danger'),
+        duped = al.length && al.text().indexOf('duplicate') > -1;
+    
+    $(data).find('.row').last().find('table tr').each(function() {
+        if ($(this).find('td').length) {
+            var lastseen = $(this).find('td').eq(lastseencol),
+                owner = $(this).find('td').eq(ownercol).text(),
+                lastseendate = new Date(lastseen.text()),
+                lastseenrawdate = lastseen.text();
+                hasSales = previousOwner && previousOwner != owner;
+            
+            if (hasSales) {
+                days = _dayDifference(lastseendate, now);
+                
+                recentsale = days <= 90;
+            }
+            
+            previousDate = lastseendate;
+            previousOwner = owner;
+        } else if ($(this).find('th').length) {
+            $(this).find('th').each(function() {
+                switch ($(this).text()) {
+                    case 'Item ID':
+                        itemidcol = $(this).index();
+                        break;
+                    case 'Last Seen':
+                        lastseencol = $(this).index();
+                        break;
+                    case 'Owner':
+                        ownercol = $(this).index();
+                        break;
+                }
+            });
+        }
+        
+        return !hasSales;
+    });
+    
+    var result = $('<span class="label"/>'), labelClass = 'label-danger';
+    
+    if (days || days == 0) {
+        var text;
+        
+        if (days == 0) {
+            text = 'Today';
+        } else {
+            var da = days == 1 ? 'day' : 'days';
+            
+            text = days + ' ' + da + ' ago';
+        }
+        
+        if (duped) {
+            text += '*';
+        }
+        
+        result.text(text);
+        
+        if (days <= 60) {
+            labelClass = 'label-success';
+        } else if (days <= 90) {
+            labelClass = 'label-warning';
+        }
+    } else {
+        result.text('None');
+    }
+    
+    result.addClass(labelClass);
+    
+    label.replaceWith(result);
+}
+
+_dayDifference = function(d1, d2) {
+    var oneDay = 24*60*60*1000;
+
+    return Math.round(Math.abs((d1.getTime() - d2.getTime())/(oneDay)))
+}
+
+_itemURL = function(id) {
+    return window.location.protocol + '//' + window.location.hostname + '/item/' + (id || '');
+}
+
+$(document).ready(function() {
+   _ready(); 
+});
