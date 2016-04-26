eaeb7f519e86995787942699a1323a979e89e320
The Oddball <oddball@oddball.tf>
2016-04-26 11:38:24 -0700

    Update backpacktf-premium-sales-finder.meta.js

--- backpacktf-premium-sales-finder.meta.js
+++ backpacktf-premium-sales-finder.meta.js
@@ -5,7 +5,7 @@
 // @description Find recent sales for those hats :3
 // @include     /https?:\/\/backpack\.tf\/premium\/search.*/
 // @include     /https?:\/\/backpack\.tf\/item\/.*/
-// @version     2.3
+// @version     2.4
 // @grant       none
 // @updateURL   https://github.com/juliarose/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.meta.js
 // @downloadURL https://github.com/juliarose/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.user.js
